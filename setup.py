0d0a811c02fc7e511efd7a550efc4fe394207db2
Jarek Śmiejczak <bcfh@seq.pl>
2012-03-18 18:47:02 +0100

    package meta updated

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup, find_packages
+try:
+    import pstats
+except ImportError:
+    import sys
+    print "You must install pstat/profiler package"
+    sys.exit(-1)
 
 setup(
     name= "hrdns",
@@ -7,9 +13,9 @@ setup(
     install_requires= ['pyquery', 'pyCli'],
     scripts= ['hrdns/bin/hrobot_dns.py',],
     author= 'Jarek S.',
-    author_email= 'jarekps@sysd.pl',
+    author_email= 'bcfh@seq.pl',
     description= 'Small tool created for management of hetzner dns service from commandline',
     license= 'X11',
     keywords= 'hetzner dns management commandline pycli',
-    url= 'http://github.com/jareks/hrdns',
+    url= 'http://github.com/devop/hrdns',
 )
