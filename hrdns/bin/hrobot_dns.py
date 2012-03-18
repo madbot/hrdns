f3844814b6a2cde475781da0b4c64a2f836c1765
Jarek Śmiejczak <bcfh@seq.pl>
2012-03-18 16:27:02 +0100

    moved helpdoc

--- hrdns/bin/hrobot_dns.py
+++ hrdns/bin/hrobot_dns.py
@@ -4,7 +4,11 @@ import hrdns.commands
 import time
 
 allowed_modules = ('add', 'show', 'update', 'delete', 'list')
-
+help_doc = """
+Select module from list and call: hrobot_dns.py <module_name> --params
+Available modules: %s" % ', '.join(allowed_modules
+Example usage https://github.com/jareks/hrdns/blob/master/README
+"""
 if __name__ == '__main__':    
     if len(sys.argv) >= 2:
         module_name = sys.argv[1]
@@ -15,7 +19,5 @@ if __name__ == '__main__':
         else:
             print "Not found module called %s. Choose from %s." % (module_name, ', '.join(allowed_modules))
             sys.exit(-1)
-    print "Select module from list and call: hrobot_dns.py <module_name> --params"
-    print "Available modules: %s" % ', '.join(allowed_modules)
-    print "Read https://github.com/jareks/hrdns/blob/master/README"
+    print help_doc
     sys.exit(-1)
